97412cf65a49d5b682274cca7eb2f9ebc92ccfff
Admin <admin@Admins-MacBook-Pro.local>
2026-08-17 21:33:36 +0100

    home

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -2,3 +2,4 @@ export { default as BurgerIcon } from './burger.astro';
 export { default as EmailIcon } from './email.astro';
 export { default as PhoneIcon } from './phone.astro';
 export { default as WhatsappIcon } from './whatsapp.astro';
+export { default as HeartIcon } from './heart.astro';
