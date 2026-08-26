2e41d663ac58fed3f7c027fce7798576843638dd
Admin <admin@Admins-MacBook-Pro.local>
2026-08-26 11:44:33 +0100

    mobile menu fixes

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,4 +1,5 @@
 export { default as BurgerIcon } from './burger.astro';
+export { default as CloseIcon } from './close.astro';
 export { default as EmailIcon } from './email.astro';
 export { default as PhoneIcon } from './phone.astro';
 export { default as WhatsappIcon } from './whatsapp.astro';
